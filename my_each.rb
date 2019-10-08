036c0a6c40e1ece9e6d1c16817b8289c3e6e71e3
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-08 17:44:56 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,5 @@
 
-def my_each(array) 
+=begin def my_each(array) 
   counter = 0
   while counter < array.length
     yield(array[counter]) # return iteration eg. array[0], array[1], array[2]
@@ -9,3 +9,15 @@ def my_each(array)
 end
 
 my_each(["a", "b", "c", "d"]) {|word| return "#{word}"} 
+=end
+
+def my_each(array) 
+  counter = 0
+  while counter < array.length
+    yield(array[counter]) # return iteration eg. array[0], array[1], array[2]
+    counter += 1 # increment counter by 1
+  end
+  return array #return array
+end
+
+my_each(["Hi", "how", "are", "you", "?"]) {|word| print "#{word}\n"} 
