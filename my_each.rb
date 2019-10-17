7dc04494c58c4277c1bcc8cf2d414a55f3450da5
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-17 04:04:04 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+def my_each (array)
+  i = 0
+  while array.length < i
+   i * 2
+   i += 1
+  end  
+end
+
+my_each([2,4,6,8]) do |i|
+  yield i 
 end
