b22c9eb0618b2b9b92cd5b60bc8ab971a82c5272
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-17 04:19:06 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -2,13 +2,9 @@ require 'pry'
 
 def my_each (array)
   i = 0
-  while array.length < i
-   i * 2
-   i += 1
-   yield
+  while i < array.length
+  yield array[i]
+  i += 1
   end  
-end
-
-my_each([2,4,6,8]) do |i|
-  yield i 
+  array
 end
